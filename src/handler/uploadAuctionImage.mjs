358b4c9917dd57e70be9f1aed71f4b9363e2fab2
princekumar910 <princekumar007p@gmail.com>
2025-04-21 17:52:18 +0530

    complete auction services

--- src/handler/uploadAuctionImage.mjs
+++ src/handler/uploadAuctionImage.mjs
@@ -10,11 +10,28 @@ const dynamodbClient = new aws.DynamoDB.DocumentClient({
 
 async function uploadAuctionImage(event) {
     const { id } = event.pathParameters; 
+    const {email} = JSON.parse(event.requestContext.authorizer.claims)
 //  handle the case where auction does not exist
 try {
     
     const auction = await getAuctionById(id) ;
+    if(auction.seller !== email){
+        return {
+            statusCode:404,
+            body: JSON.stringify({ Error: "You are not authorize !" }),
+        };
+    }
+    // check if auction exists apply the logic to upload image
+    const base64Regex = /^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=)?$/
+    if(!base64Regex.test(event.body)){
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ Error: "Invalid base64 string" }),
+        };
+    }
+
     const base64 = event.body.replace(/^data:image\/\w+;base64,/, ''); 
+    
     const buffer = Buffer.from(base64, 'base64');
     const result = await uploadImageToS3(`images/${id}.jpg`, buffer); 
     const params =  {
